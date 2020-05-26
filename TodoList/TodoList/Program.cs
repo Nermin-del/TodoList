2ea5a656ca6ab99e833fce8207d25066e886a00c
Nermin-del <59168189+Nermin-del@users.noreply.github.com>
2020-05-26 21:02:40 +0200

    update

---
+++ TodoList/TodoList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TodoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*todo: 1. Fixa alternativ. 1an ska ha funktionen att lägga till todo.  2an ska generera alternativet siffror som håller antal varor. 
+             * 3an ska ha alternativet att kunna markera todo som färdig. 4an ska kunna ta bort oösnkade saker på listan. 5an kommer ha funktionen att avsluta program.
+             en funktion som ger ett fel om man skriver in fel input ska även läggas in. Behöver en lista. Göra begränsningar.*/
+            string input;
+            bool runProgram = true;
+
+            Console.WriteLine("välkommen till din Todo-app");
+           
+        }
+    }
+}
